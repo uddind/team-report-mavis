d77d883b1c4416bbe64213c5dc05a8d086eb0f58
uddind <qomarudinmohammad9@gmail.com>
2026-07-06 09:46:49 +0700

    Shroten app name to Mavis Report

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.mavis.teamreport',
-  appName: 'TeamReport Mavis',
+  appId: 'com.teamreport',
+  appName: 'ReportMavis',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
